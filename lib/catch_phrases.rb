8605062153daf52c555b5b38b9d180430aa1eed3
Manoj Patel <nycdays300@gmail.com>
2020-01-25 00:12:58 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,11 +1,4 @@
 
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase = 'It's-a me, Mario!'
- end
-
-
-
-def toadstool
-  puts status
+def catch_phrase(phrase)
+  puts phrase
 end
